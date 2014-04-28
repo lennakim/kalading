9d310af17b1c4c7f3fc189e6dc49caa4a18f7baf
nanami <wangzhe.nanami@gmail.com>
2014-04-28 16:46:34 +0800

    Remove unused files

--- db/backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /mnt/kalading/db/data
-./export_kalading.bat
-cd ..
-tar zcf $(date +%Y%m%d).tar.gz data/ 
-scp $(date +%Y%m%d).tar.gz root@115.28.132.220:/root/dbbackup/kalading_db.tar.gz
--- utils/fetch_database_from_tokyo.bat
+++
@@ -1,4 +0,0 @@
-cd ../db
-ssh ubuntu@54.248.126.168 "cd /mnt/kalading/db/data; rm -f *.json; ./export_kalading.bat; cd ..; tar zcf data.tar.gz data"
-scp -i ../tokyo_new.ppk ubuntu@54.248.126.168:/mnt/kalading/db/data.tar.gz ./
-bsdtar zxf data.tar.gz && cd .. && rake db:reseed && cd db/data/ && import_kalading.bat
