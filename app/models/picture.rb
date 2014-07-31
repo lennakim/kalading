626a35ca405659bb189c58ec568990042db07bb7
Qin HuaiYu <646228839@qq.com>
2014-07-31 03:14:57 +0000

    增加液油及电瓶照片接口，picture文件的修改

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -9,20 +9,15 @@ class Picture
   embedded_in :tool_record, :inverse_of => :pictures
   embedded_in :iamge_text, :inverse_of => :pictures
   embedded_in :maintain, :inverse_of => :outlook_pics
-  embedded_in :maintain, :inverse_of => :auto_front_pics
-  embedded_in :maintain, :inverse_of => :auto_back_pics
-  embedded_in :maintain, :inverse_of => :auto_left_pics
-  embedded_in :maintain, :inverse_of => :auto_right_pics
-  embedded_in :maintain, :inverse_of => :auto_top_pics
-  embedded_in :maintain, :inverse_of => :old_oil_pics
-  embedded_in :maintain, :inverse_of => :oil_filter_pics
-  embedded_in :maintain, :inverse_of => :old_air_filter_pics
-  embedded_in :maintain, :inverse_of => :old_cabin_filter_pics
+  embedded_in :maintain, :inverse_of => :oil_pics
+  embedded_in :maintain, :inverse_of => :air_filter_pics
+  embedded_in :maintain, :inverse_of => :cabin_filter_pics
   embedded_in :maintain, :inverse_of => :tire_left_front_pics
   embedded_in :maintain, :inverse_of => :tire_left_back_pics
   embedded_in :maintain, :inverse_of => :tire_right_front_pics
   embedded_in :maintain, :inverse_of => :tire_right_back_pics
   embedded_in :maintain, :inverse_of => :tire_spare_pics
+  embedded_in :maintain, :inverse_of => :oil_and_battery_pics
 
   has_mongoid_attached_file :p, :styles => { small: 'x100' }, :url => "/system/:class/:id/p/:style.:extension"
 end
