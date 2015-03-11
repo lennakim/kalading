9efc8571c9afb8018b9b3b289c677fbbb30833b7
qin140524 <646228839@qq.com>
2015-03-11 13:39:02 +0800

    城市库管能导出所在仓库的库存情况，不导出价格

--- app/models/storehouse.rb
+++ app/models/storehouse.rb
@@ -22,9 +22,9 @@ class Storehouse
   
   def to_csv(options = {})
     CSV.generate(options) do |csv|
-      csv << [I18n.t(:part_brand), I18n.t(:part_number), I18n.t(:part_type), I18n.t(:in_quantity), I18n.t(:remained_quantity), I18n.t(:batch_price), I18n.t(:sell_price_history), I18n.t(:remark)]
+      csv << [I18n.t(:part_brand), I18n.t(:part_number), I18n.t(:part_type), I18n.t(:in_quantity), I18n.t(:remained_quantity), I18n.t(:remark)]
       self.partbatches.group_by(&:part).to_a.sort_by {|x, y| x.number }.each do |part, partbatches|
-        csv << [part.part_brand.name, part.number, part.part_type.name, partbatches.sum {|x| x.quantity}, partbatches.sum {|x| x.remained_quantity}, partbatches.first.price, part.ref_price, part.remark ]
+        csv << [part.part_brand.name, part.number, part.part_type.name, partbatches.sum {|x| x.quantity}, partbatches.sum {|x| x.remained_quantity}, part.remark ]
       end
     end
   end
