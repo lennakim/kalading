c369459471cb93e6e414948deaa8082d926a85c1
qin140524 <646228839@qq.com>
2015-03-13 00:56:48 +0800

    全国库管可以看订单统计

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -44,6 +44,7 @@ class Ability
     if user.roles.include? ROLE_ID('national_storehouse_admin')
       can [:manage_all, :part_transfer, :part_transfer_to, :part_yingyusunhao, :do_part_yingyusunhao, :statistics], Storehouse
       can [:create, :update, :destroy], [Storehouse, Partbatch, Part, PartType, PartBrand, Supplier]
+      can :order_stats, Order
     end
     
     if user.roles.include? ROLE_ID('data_admin')
