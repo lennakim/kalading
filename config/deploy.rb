4c2ca619e8cb644b873b6639e5b0e172a7baa89a
shooter <shooter.hero@gmail.com>
2015-03-18 15:21:11 +0800

    取消  shared public

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :puma_role, :app
 set :linked_files, %w{config/database.yml config/mongoid.yml}
 
 # dirs we want symlinking to shared
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public }
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle }
 
 namespace :deploy do
   after :finishing, 'deploy:cleanup'
