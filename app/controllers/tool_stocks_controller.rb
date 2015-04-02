39963efdcb9d11ee3ede08443f1043a27a113f30
Jian Lin <linjian815@gmail.com>
2015-04-02 15:18:42 +0800

    添加字段 tool_type_category 到 ToolStock 库存信息页面查询功能

--- app/controllers/tool_stocks_controller.rb
+++ app/controllers/tool_stocks_controller.rb
@@ -3,6 +3,19 @@ class ToolStocksController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @tool_stocks = ToolStock.page(params[:page]).per(20)
+    criteria = ToolStock
+    if params[:remained_ceiling].present?
+      criteria = criteria.where(:remained_count.lte => params[:remained_ceiling])
+    end
+
+    if params[:city_id].present?
+      criteria = criteria.where(city_id: params[:city_id])
+    end
+
+    if params[:category].present?
+      criteria = criteria.where(tool_type_category: params[:category])
+    end
+
+    @tool_stocks = criteria.page(params[:page]).per(20)
   end
 end
--- app/models/tool_stock.rb
+++ app/models/tool_stock.rb
@@ -4,6 +4,7 @@ class ToolStock
 
   field :assigned_count, type: Integer, default: 0
   field :remained_count, type: Integer, default: 0
+  field :tool_type_category, type: String
 
   attr_accessible :tool_type_id, :city_id
 
@@ -14,4 +15,10 @@ class ToolStock
   validates :remained_count, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   validates_presence_of :tool_type_id, :city_id
   validates_uniqueness_of :tool_type_id, scope: :city_id
+
+  before_create :set_tool_type_category
+
+  def set_tool_type_category
+    self.tool_type_category = tool_type.category
+  end
 end
