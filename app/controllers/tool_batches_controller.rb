645d5984fd5f7a68b224c878a1fb2301aeabedc2
Jian Lin <linjian815@gmail.com>
2015-04-02 11:28:43 +0800

    添加字段 tool_type_category 到 ToolBatch 进货信息页面查询功能

--- app/controllers/tool_batches_controller.rb
+++ app/controllers/tool_batches_controller.rb
@@ -3,7 +3,26 @@ class ToolBatchesController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @tool_batches = ToolBatch.page(params[:page]).per(20)
+    criteria = ToolBatch
+    if params[:date_from].present?
+      date_from = Date.parse(params[:date_from]).beginning_of_day
+      criteria = criteria.where(:created_at.gte => date_from)
+    end
+
+    if params[:date_to].present?
+      date_to = Date.parse(params[:date_to]).end_of_day
+      criteria = criteria.where(:created_at.lte => date_to)
+    end
+
+    if params[:category].present?
+      criteria = criteria.where(tool_type_category: params[:category])
+    end
+
+    if params[:city_id].present?
+      criteria = criteria.where(city_id: params[:city_id])
+    end
+
+    @tool_batches = criteria.page(params[:page]).per(20)
   end
 
   def new
--- app/models/tool_batch.rb
+++ app/models/tool_batch.rb
@@ -7,6 +7,7 @@ class ToolBatch
   # 批次/规格
   field :spec, type: String
   field :note, type: String
+  field :tool_type_category, type: String
 
   attr_accessible :quantity, :price, :spec, :note, :tool_type_id, :city_id
 
@@ -19,11 +20,15 @@ class ToolBatch
   validates :price, numericality: { greater_than: 0 }
   validates_presence_of :tool_type_id, :city_id, :operator_id
 
-  before_create :increase_tool_stock
+  before_create :increase_tool_stock, :set_tool_type_category
 
   def increase_tool_stock
     tool_stock = ToolStock.where(city_id: city.id, tool_type_id: tool_type.id).first_or_create!
     tool_stock.inc(:remained_count, self.quantity)
     self.tool_stock = tool_stock
+  end
+
+  def set_tool_type_category
+    self.tool_type_category = tool_type.category
   end
 end
