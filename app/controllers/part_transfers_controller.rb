75076647e21c32c08b9fe274e03a565c61bf16bc
nanami-dev <nanami@linux-7bht.site>
2015-04-21 11:30:13 +0800

    调货支持导出

--- app/controllers/part_transfers_controller.rb
+++ app/controllers/part_transfers_controller.rb
@@ -17,6 +17,18 @@ class PartTransfersController < InheritedResources::Base
       @part_transfers = @part_transfers.where(target_sh: Storehouse.find(params[:target_sh]))
     end
     @part_transfers = @part_transfers.page params[:page]
+    if params[:export].present?
+      csv_data = CSV.generate do |csv|
+        a = [I18n.t(:brand), I18n.t(:manuf_number), I18n.t(:part_type), I18n.t(:quantity), I18n.t(:to), I18n.t(:created_at), I18n.t(:state)]
+        csv << a
+        @part_transfers.each do |pt|
+          csv << [pt.part.part_brand.name, pt.part.number, pt.part.part_type.name, pt.quantity, pt.target_sh.name, I18n.l(pt.created_at), I18n.t(pt.aasm_state)]
+        end
+      end
+      headers['Last-Modified'] = Time.now.httpdate
+      send_data csv_data, :filename => I18n.t(:part_transfer)+ I18n.l(DateTime.now.to_date) + '.csv'
+      return
+    end
     index!
   end
   
