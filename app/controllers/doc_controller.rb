04480eaf1410698f8ea456b2bdf2abc95afb8c35
shooter <shooter.hero@gmail.com>
2015-04-28 13:41:08 +0800

    取消验证

--- app/controllers/doc_controller.rb
+++ app/controllers/doc_controller.rb
@@ -1,10 +1,5 @@
 class DocController < ApplicationController
-
   layout "api"
-
-  if Rails.env.production? || Rails.env.staging?
-    http_basic_authenticate_with name: "kalading", password: "secret"
-  end
 
   def v2
 
